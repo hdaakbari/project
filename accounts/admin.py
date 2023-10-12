150437b44d370bdd702994821f2167306063b860
hda <hda.akbari@gmail.com>
2023-10-12 13:39:36 +0330

    'r02'

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from accounts.models import *
-from .models import UserProfile
+from .models import UserProfile,Education,Experience
 
 admin.site.register(UserProfile)
+admin.site.register(Education)
+admin.site.register(Experience)
--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,20 @@ class UserProfile(models.Model):
     bio=models.CharField(max_length=50)
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     picture=models.ImageField(upload_to='profile_picture')
+    
+class Education(models.Model):
+    school=models.CharField(max_length=100)
+    degree=models.CharField(max_length=100)
+    fos=models.CharField(max_length=100)
+    start=models.DateTimeField()
+    end=models.TextField()
+    desc=models.TextField()
+    
+class Experience(models.Model):
+    title=models.CharField(max_length=100)
+    compony=models.CharField(max_length=100)
+    location=models.CharField(max_length=100)
+    start=models.DateTimeField()
+    end=models.DateField()
+    description=models.TextField()
+    
